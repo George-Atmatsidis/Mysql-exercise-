882718fc5546927d3f1979314256b3ad338f2d64
Giorgos Atmatsidis <georgatma.gr@gmail.com>
2018-03-15 03:13:39 +0200

    Update georgios_atmatsidis_3696.sql
    AP1 FIXED

--- AP1/Giorgos Atma/georgios_atmatsidis_3696.sql
+++ AP1/Giorgos Atma/georgios_atmatsidis_3696.sql
@@ -3,16 +3,18 @@
 ΕΠΩΝΥΜΟ: ΑΤΜΑΤΣΙΔΗΣ
 ΑΕΜ: 3696
 Email: georgatma.gr@gmail.com
+PDF:ΑΣΚΗΣΗ ΠΡΑΞΗΣ Ι: ΕΝTOΛΕΣ OΡΙΣΜΟΥ ΔΕΔΟΜΕΝΩΝ (DDL)
+Βελτιωμένη-Διορθωμένη έκδοση εργασίας
 */
-CREATE DATABASE atmatsidis; 
-USE atmatsidis;
+CREATE DATABASE askisi_praksis_1; 
+USE askisi_praksis_1;
 ###############################################################################################################
 # ΑΣΚΗΣΗ 1
 ###############################################################################################################
 CREATE TABLE EMPLOYEES( 
 FNAME VARCHAR(15), 
 LNAME VARCHAR(15),
-SSN INT(7) NOT NULL,
+SSN CHAR(7) NOT NULL,
 PRIMARY KEY (SSN)
 );
 SHOW CREATE TABLE EMPLOYEES;
@@ -22,7 +24,7 @@ SHOW CREATE TABLE EMPLOYEES;
 EMPLOYEES, CREATE TABLE `EMPLOYEES` (
   `FNAME` varchar(15) DEFAULT NULL,
   `LNAME` varchar(15) DEFAULT NULL,
-  `SSN` int(7) NOT NULL,
+  `SSN` char(7) NOT NULL,
   PRIMARY KEY (`SSN`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 */
@@ -41,12 +43,13 @@ ALTER TABLE EMPLOYEES ADD COLUMN(
 EMPLOYEES, CREATE TABLE `EMPLOYEES` (
   `FNAME` varchar(15) DEFAULT NULL,
   `LNAME` varchar(15) DEFAULT NULL,
-  `SSN` int(7) NOT NULL,
+  `SSN` char(7) NOT NULL,
   `ADDRESS` varchar(40) DEFAULT NULL,
   `ADDRESS2` varchar(40) DEFAULT NULL,
   `SALARY` int(11) DEFAULT NULL,
   PRIMARY KEY (`SSN`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1              
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+             
 */
 ###############################################################################################################
 #ΑΣΚΗΣΗ 3
@@ -63,25 +66,24 @@ SHOW CREATE TABLE EMPLOYEES;
 EMPLOYEES, CREATE TABLE `EMPLOYEES` (
   `FNAME` varchar(15) DEFAULT NULL,
   `LNAME` varchar(15) DEFAULT NULL,
-  `SSN` int(7) NOT NULL,
+  `SSN` char(7) NOT NULL,
   `ADDRESS` varchar(250) DEFAULT NULL,
   `SALARY` decimal(8,2) DEFAULT NULL,
   PRIMARY KEY (`SSN`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
-
 */
 ###############################################################################################################
 #ΑΣΚΗΣΗ 4 
 ###############################################################################################################
 /*
-Λόγο της ολικής συμμετοχής στο Department ο πίνακας Employees θα δώσει το πρωτεύων κλειδί του ως ξένο στον Department.
+Λόγο της ολικής συμμετοχής στο Department ο πίνακας Employees θα δώσει το πρωτεύον κλειδί του ως ξένο στον Department.
 Για αυτό θα πρέπει να έχω  ένα γνώρισμα στον Department το EMPNO.
 Αρχικά θα φτιάξω πρώτα τον πίνακα DEPARTMENTS μέτα την σχέση MANAGE (επείδη έχει Γνώρισμα) και τέλος θα βάλω τους περιορισμούς.
 */
 CREATE TABLE DEPARTMENTS( 
 DNUMBER INTEGER NOT NULL, 
 DNAME VARCHAR(30) NOT NULL, 
-EMPNO INT(7) NOT NULL,
+EMPNO CHAR(7) NOT NULL,
 PRIMARY KEY (DNUMBER)
 );
 SHOW CREATE TABLE DEPARTMENTS;
@@ -91,15 +93,16 @@ SHOW CREATE TABLE DEPARTMENTS;
 DEPARTMENTS, CREATE TABLE `DEPARTMENTS` (
   `DNUMBER` int(11) NOT NULL,
   `DNAME` varchar(30) NOT NULL,
-  `EMPNO` int(7) NOT NULL,
+  `EMPNO` char(7) NOT NULL,
   PRIMARY KEY (`DNUMBER`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
 */
 
 # Πίνακας Σχέσης EMPLOYEE-DEPARTMENTS
 CREATE TABLE MANAGE (
 	MGSTARTDATE DATE NOT NULL,
-	EMPNO INT(7) NOT NULL,
+	EMPNO CHAR(7) NOT NULL,
 	DNUMBER INTEGER NOT NULL,
 	PRIMARY KEY (MGSTARTDATE)
 );
@@ -109,7 +112,7 @@ SHOW CREATE TABLE MANAGE;
 # Table, Create Table
 MANAGE, CREATE TABLE `MANAGE` (
   `MGSTARTDATE` date NOT NULL,
-  `EMPNO` int(7) NOT NULL,
+  `EMPNO` char(7) NOT NULL,
   `DNUMBER` int(11) NOT NULL,
   PRIMARY KEY (`MGSTARTDATE`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
@@ -127,7 +130,7 @@ SHOW CREATE TABLE DEPARTMENTS;
 DEPARTMENTS, CREATE TABLE `DEPARTMENTS` (
   `DNUMBER` int(11) NOT NULL,
   `DNAME` varchar(30) NOT NULL,
-  `EMPNO` int(7) NOT NULL,
+  `EMPNO` char(7) NOT NULL,
   PRIMARY KEY (`DNUMBER`),
   KEY `departments_const` (`EMPNO`),
   CONSTRAINT `departments_const` FOREIGN KEY (`EMPNO`) REFERENCES `EMPLOYEES` (`SSN`)
@@ -140,7 +143,7 @@ SHOW CREATE TABLE MANAGE;
 # Table, Create Table
 MANAGE, CREATE TABLE `MANAGE` (
   `MGSTARTDATE` date NOT NULL,
-  `EMPNO` int(7) NOT NULL,
+  `EMPNO` char(7) NOT NULL,
   `DNUMBER` int(11) NOT NULL,
   PRIMARY KEY (`MGSTARTDATE`),
   KEY `manage_const` (`EMPNO`),
@@ -148,19 +151,21 @@ MANAGE, CREATE TABLE `MANAGE` (
   CONSTRAINT `manage_const` FOREIGN KEY (`EMPNO`) REFERENCES `EMPLOYEES` (`SSN`),
   CONSTRAINT `manage_const1` FOREIGN KEY (`DNUMBER`) REFERENCES `DEPARTMENTS` (`DNUMBER`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
 */
 ###############################################################################################################
 #ΑΣΚΗΣΗ 5
 ###############################################################################################################
-/*Δεν γίνεται η διαγράφη του πίνακα EMPLOYEES γιατί δίνει το κλειδί του στο DEPARTMENTS.
-για να γίνει η διαγραφή θα πρέπει να διαγράψω τον περιορισμό departments_const
+/*Δεν γίνεται η διαγράφη του πίνακα EMPLOYEES γιατί δίνει το κλειδί του στο DEPARTMENTS και στον πίνακα MANAGE.
+για να γίνει η διαγραφή θα πρέπει να διαγράψω τον περιορισμό departments_const,manage_const
 Δεν εκτελώ την εντολή γιατί θα χρειαστώ τον πίνακα EMPLOYEES για την επόμενη άσκηση.
 Κατά την εκτέλεση::
 Προσπαθώντας να σβήσω το EMPLOYEES παίρνω το μηνυμα:
 	DROP TABLE EMPLOYEES Error Code: 1451. Cannot delete or update a parent row: a foreign key constraint fails
-Για να μπορέσω να κάνω διαγραφή τον πίνακα EMPLOYEES θα πρέπει να βγάλω το ξένο κλειδί (foreign key):
+Για να μπορέσω να κάνω διαγραφή τον πίνακα EMPLOYEES θα πρέπει να βγάλω τα ξένα κλειδιά (foreign key):
 
 	ALTER TABLE DEPARTMENTS DROP FOREIGN KEY departments_const;
+	ALTER TABLE MANAGE DROP FOREIGN KEY manage_const;
     
 Τώρα μπορώ να κάνω διαγραφή τον πίνακα EMPLOYEES
 
@@ -171,7 +176,6 @@ MANAGE, CREATE TABLE `MANAGE` (
 #ΑΣΚΗΣΗ 6
 ###############################################################################################################
 #Ο EMPLOYEES σημμετέχει ολικά στο DEPARTMENTS.
-#
 ALTER TABLE EMPLOYEES ADD DNO INTEGER NOT NULL; # Δημιουργώ ένα καινούργιο γνώρισμα που θα δεχτεί το ξένο κλειδί.
 ALTER TABLE EMPLOYEES ADD CONSTRAINT employees_const FOREIGN KEY (DNO) REFERENCES DEPARTMENTS (DNUMBER); # Δημιουργία περιορισμού
 SHOW CREATE TABLE EMPLOYEES;
@@ -181,7 +185,7 @@ SHOW CREATE TABLE EMPLOYEES;
 EMPLOYEES, CREATE TABLE `EMPLOYEES` (
   `FNAME` varchar(15) DEFAULT NULL,
   `LNAME` varchar(15) DEFAULT NULL,
-  `SSN` int(7) NOT NULL,
+  `SSN` char(7) NOT NULL,
   `ADDRESS` varchar(250) DEFAULT NULL,
   `SALARY` decimal(8,2) DEFAULT NULL,
   `DNO` int(11) NOT NULL,
@@ -205,17 +209,18 @@ SHOW CREATE TABLE EMPLOYEES;
 EMPLOYEES, CREATE TABLE `EMPLOYEES` (
   `FNAME` varchar(15) DEFAULT NULL,
   `LNAME` varchar(15) DEFAULT NULL,
-  `SSN` int(7) NOT NULL,
+  `SSN` char(7) NOT NULL,
   `ADDRESS` varchar(250) DEFAULT NULL,
   `SALARY` decimal(8,2) DEFAULT NULL,
   PRIMARY KEY (`SSN`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
 */
 ###############################################################################################################
 #ΑΣΚΗΣΗ 8
 ###############################################################################################################
 CREATE TABLE PROJECT(
-DNUMBER INT(7) NOT NULL,
+DNUMBER CHAR(7) NOT NULL,
 PRIMARY KEY (DNUMBER)
 );
 SHOW CREATE TABLE PROJECT;
@@ -223,14 +228,14 @@ SHOW CREATE TABLE PROJECT;
 				::----->OUTPUT<-----::
 # Table, Create Table
 PROJECT, CREATE TABLE `PROJECT` (
-  `DNUMBER` int(7) NOT NULL,
+  `DNUMBER` char(7) NOT NULL,
   PRIMARY KEY (`DNUMBER`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 */
-#  Επειδή είναι πολλά προς πολλά η Σχέση works_on θα γίνει πίνακας.
+#Επειδή είναι πολλά προς πολλά η Σχέση works_on θα γίνει πίνακας.
 CREATE TABLE WORKS_ON(
-PROJECT_ID INT (7) NOT NULL,
-EMPNO INT(7) NOT NULL,
+PROJECT_ID CHAR (7) NOT NULL,
+EMPNO CHAR(7) NOT NULL,
 PRIMARY KEY(PROJECT_ID,EMPNO),
 FOREIGN KEY (PROJECT_ID) REFERENCES PROJECT(DNUMBER),
 FOREIGN KEY (EMPNO) REFERENCES EMPLOYEES(SSN)
@@ -240,13 +245,13 @@ SHOW CREATE TABLE WORKS_ON;
 				::----->OUTPUT<-----::
 # Table, Create Table
 WORKS_ON, CREATE TABLE `WORKS_ON` (
-  `PROJECT_ID` int(7) NOT NULL,
-  `EMPNO` int(7) NOT NULL,
+  `PROJECT_ID` char(7) NOT NULL,
+  `EMPNO` char(7) NOT NULL,
   PRIMARY KEY (`PROJECT_ID`,`EMPNO`),
   KEY `EMPNO` (`EMPNO`),
   CONSTRAINT `WORKS_ON_ibfk_1` FOREIGN KEY (`PROJECT_ID`) REFERENCES `PROJECT` (`DNUMBER`),
   CONSTRAINT `WORKS_ON_ibfk_2` FOREIGN KEY (`EMPNO`) REFERENCES `EMPLOYEES` (`SSN`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
-*/
 
-#drop database atmatsidis;
+*/
+#drop database askisi_praksis_1;
